0acbd59cd359f88f2c70f44f205c1f606f98c7d2
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-08-05 10:45:37 +0530

    Update peak-element.cpp

--- Difficulty: Medium/Peak element/peak-element.cpp
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -20,8 +20,9 @@ class Solution {
         int mid;
         
         while (lo<=hi){
+            mid = lo+(hi-lo)/2;
             if (arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
-            else if (arr[mid] > arr[mid-1]) lo = mid+1;
+            else if (arr[mid] < arr[mid+1]) lo = mid+1;
             else hi = mid-1;
         }
         return -1;

9ffb27c6527f8866814ed1ada6d6eb51c5eed31a
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-08-05 10:28:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        // code here
+        
+        // if size is 1 then it will return the 0th index
+        if (arr.size() == 1) return 0;
+        
+        // if the first element is greater than the second it will return 
+        // 0th index because if it is decreasing from the starting then the
+        // first element will always be the peak element
+        if (arr[0] > arr[1]) return 0;
+        
+        
+        // it will return the last element
+        if (arr[arr.size()-2] < arr[arr.size()-1]) return arr.size()-1;
+        
+        int lo = 0;
+        int hi = arr.size()-1;
+        int mid;
+        
+        while (lo<=hi){
+            if (arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
+            else if (arr[mid] > arr[mid-1]) lo = mid+1;
+            else hi = mid-1;
+        }
+        return -1;
+    }
+};

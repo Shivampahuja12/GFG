216f440153b9d8c504c6778f4a9b834a609f4ac5
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-05-23 09:48:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.java
+++ Difficulty: Easy/Identical Trees/identical-trees.java
@@ -15,8 +15,6 @@ class Solution {
         if (r1 == null && r2 == null) return true;
         if (r1 == null || r2 == null) return false;
         if (r1.data != r2.data) return false;
-        if (!isIdentical(r1.left, r2.left)) return false;
-        if (!isIdentical(r1.right, r2.right)) return false;
-        return true;
+        return isIdentical(r1.left, r2.left) && isIdentical(r1.right, r2.right);
     }
 }

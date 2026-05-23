71722d37503d0068327cfbb314ee96197cb43bd6
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-05-23 12:22:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Symmetric Tree/symmetric-tree.java
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -13,26 +13,12 @@ class Node{
 
 class Solution {
     public boolean isSymmetric(Node root) {
-        // code here
-        mirror(root.left);
-        return isIdentical(root.left, root.right);
+        return symmetric(root.left, root.right);
     }
-    void mirror(Node root) {
-        // code here
-        if (root == null) return;
-        Node temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        mirror(root.left);
-        mirror(root.right);
-    }
-    public boolean isIdentical(Node r1, Node r2) {
-        // code here
+    static boolean symmetric(Node r1, Node r2){
         if (r1 == null && r2 == null) return true;
         if (r1 == null || r2 == null) return false;
         if (r1.data != r2.data) return false;
-        if (!isIdentical(r1.left, r2.left)) return false;
-        if (!isIdentical(r1.right, r2.right)) return false;
-        return true;
+        return symmetric(r1.left, r2.right) && symmetric(r1.right, r2.left);
     }
 }

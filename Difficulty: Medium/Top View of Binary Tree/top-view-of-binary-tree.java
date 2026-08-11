b8be8bcdb05f34bf53054bbe65efe95eae40d843
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-08-11 10:34:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -1,52 +1,49 @@
 /*
 class Node {
-    int data;
-    Node left, right;
-
-    Node(int val) {
-        this.data = val;
-        this.left = null;
-        this.right = null;
-    }
+	int data;
+	Node left, right;
+	
+	Node(int val) {
+		this.data = val;
+		this.left = null;
+		this.right = null;
+	}
 }
 */
-class Solution {
-    
-    class Pair {
-        Node node;
-        int hd; // horizontal distance
-        Pair(Node node, int hd){
-            this.node = node;
-            this.hd = hd;
-        }
-    }
-    
-    public ArrayList<Integer> topView(Node root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        if (root == null) return list;
-        Map<Integer, Integer> map = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(root, 0));
-        while (!q.isEmpty()){
-            Pair p = q.poll();
-            Node node = p.node;
-            int hd = p.hd;
-            
-            // If horizontal distance is seen for the first time, store it
-            if (!map.containsKey(hd)) {
-                map.put(hd, node.data);
-            }
-            
-            // Add left and right children with updated horizontal distances
-            if (node.left != null) q.add(new Pair(node.left, hd - 1));
-            if (node.right != null) q.add(new Pair(node.right, hd + 1));
-        }
-        
-        // add map values to list
-        for (int value : map.values()) {
-            list.add(value);
-        }
 
-        return list;
-    }
+class Pair {
+	Node node;
+	int line;
+	public Pair(Node node, int line) {
+		this.node = node;
+		this.line = line;
+	}
+}
+class Solution {
+	public ArrayList<Integer> topView(Node root) {
+		Map<Integer, Integer> map = new HashMap<>();
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(root, 0));
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int i = 0; i<size; i++) {
+				Pair p = q.poll();
+				Node noddy = p.node;
+				int line = p.line;
+				if (!map.containsKey(line)) {
+					map.put(line, noddy.data);
+				}
+				if (noddy.left != null)
+					q.add(new Pair(noddy.left, line - 1));
+				if (noddy.right != null)
+					q.add(new Pair(noddy.right, line + 1));
+			}
+		}
+		ArrayList<Integer> list = new ArrayList<>();
+		Map<Integer, Integer> treeMap = new TreeMap<>(map);
+		for (Map.Entry<Integer, Integer> entry: treeMap.entrySet()) {
+			list.add(entry.getValue());
+		}
+		return list;
+	}
 }

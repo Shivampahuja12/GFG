7d0be8c35a86d9ffccf79faeaa18b48a0fdf81c2
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-08-29 04:27:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -1,8 +1,16 @@
 class Solution {
-    public int nthFibonacci(int n) {
+    static int nthFibonacci(int n) {
         // code here
-        if (n==0) return 0;
-        if (n==1) return 1;
-        return nthFibonacci(n-1) + nthFibonacci(n-2);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
